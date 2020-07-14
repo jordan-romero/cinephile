95ce9aa33fcfaa199caa32ad5174b7fe8b468c5f
jordles113 <jordan.t.romero@gmail.com>
2020-07-14 16:18:56 -0500

    adds lists show and index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,3 +1,17 @@
 class ListsController < ApplicationController
-    
+    before_action :set_list, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @lists = List.all 
+    end 
+
+    def show 
+    end 
+
+
+    private 
+
+    def set_list
+        @list = List.find(params[:id])
+    end 
 end
