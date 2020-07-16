162c019029cae779e2d7bc4edee10924cdf0e34f
jordles113 <jordan.t.romero@gmail.com>
2020-07-15 22:05:14 -0500

    adds full crud for lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,10 +8,46 @@ class ListsController < ApplicationController
     def show 
     end 
 
+    def new 
+        @list = List.new
+    end 
+
+    def create
+        @list = List.new(list_params)
+        if @list.save
+            redirect_to @list
+        else 
+            render 'new'
+        end 
+    end 
+
+    def edit 
+    end 
+
+    def update
+        @list.update(list_params)
+        if @list.save
+            redirect_to @list 
+        else 
+            render 'edit'
+        end 
+    end 
+
+    def destroy 
+        if @list.destroy
+            redirect_to lists_path
+        else 
+            render 'edit'
+        end 
+    end 
 
     private 
 
     def set_list
         @list = List.find(params[:id])
+    end 
+    
+    def list_params
+        params.require(:list).permit(:name, :description)
     end 
 end
