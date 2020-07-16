8dbbffb3b610bd208f87c3c0a92fba94baa0957c
jordles113 <jordan.t.romero@gmail.com>
2020-07-16 18:24:03 -0500

    establishing ability to select movies to review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,31 @@
 class ReviewsController < ApplicationController
+    
+    def index
+        @reviews = Review.all 
+    end 
+
+    def show
+        @review = Review.find(params[:id])
+    end 
+
+    def new 
+        @review = Review.new 
+    end 
+
+    def create
+        @review = Review.new(review_params)
+        if @review.save
+            redirect_to @review
+        else  
+            render 'new'
+        end 
+    end 
+
+    private 
+    
+    def review_params
+        params.require(:reviews).permit(:rating, :content, movie_attributes: [:id, :name])
+
+    end 
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :movie
-
+  accepts_nested_attributes_for :movie
 end
