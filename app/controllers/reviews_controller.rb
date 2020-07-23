0503d9c077c0dc67c055bf3cf4156b67a673f86f
jordles113 <jordan.t.romero@gmail.com>
2020-07-23 15:27:15 -0500

    adds nested route for reviews all but show is working

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,15 +2,39 @@ class ReviewsController < ApplicationController
     before_action :set_review, only: [:show, :edit, :update, :destroy]
     
     def index
-        @reviews = Review.all 
+        if params[:movie_id]
+            @movie = Movie.find_by(id: params[:movie_id])
+            if @movie.nil?
+              redirect_to movies_path, alert: "Movie not found"
+            else
+              @reviews = @movie.reviews
+            end
+        else
+            @reviews = Review.all
+        end
     end 
 
     def show
+        if params[:movie_id]
+            @movie = Movie.find_by(id: params[:movie_id])
+            @review = @movie.reviews.find_by(id: params[:id])
+            if @review.nil?
+              redirect_to movie_review_path(@movie), alert: "Movie not found"
+            end
+        else
+            @review = Review.find(params[:id])
+        end 
+    end
         
-    end 
+     
 
     def new 
-        @review = Review.new 
+        if params[:movie_id] && !Movie.exists?
+            (params[:movie_id])
+            redirect_to movies_path, alert: "Movie not Found."
+        else 
+            @review = Review.new(movie_id: params[:movie_id])
+        end 
     end 
     
     def create
@@ -24,15 +48,31 @@ class ReviewsController < ApplicationController
     end 
 
     def edit 
+        if params[:movie_id]
+            movie = Movie.find_by(id:params[:movie_id])
+            if movie.nil? 
+                redirect_to movies_path, alert: "Movie not Found."
+            else
+                @review = movie.reviews.find_by(id: params[:id])
+                redirect_to reviews_path, alert: "Review not Found." if @review.nil? 
+            end 
+        else 
+            @review = Review.find(params[:id])
+        end 
     end 
 
-    def update 
+    def update          
         @review.update(review_params)
         if @review.save 
             redirect_to @review 
         else 
             render 'edit'
         end 
+    end 
+
+    def destroy 
+        @review.destroy(review_params)
+        redirect_to reviews_path
     end 
 
     private 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :reviews
+  resources :reviews, only: [:create, :update, :destroy]
   resources :users
   resources :lists
-  resources :movies
+  resources :movies do 
+    resources :reviews, only: [:show, :new, :edit, :index]
+  end 
   root to: 'pages#home'
 
   #post "search_movies" => "movies#search", as: :search
