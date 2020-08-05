d66601382dc1512d98344ad5e753dc86dda9459c
jordles113 <jordan.t.romero@gmail.com>
2020-08-04 19:20:42 -0500

    will move to twitter auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
-
-    {
-      :image_aspect_ratio => "square",
-      :image_size => 50
-    }
+    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
 end
+
