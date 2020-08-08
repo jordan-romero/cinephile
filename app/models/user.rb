f8b82163561a72f3644a568343d9a6c2e834b574
jordles113 <jordan.t.romero@gmail.com>
2020-08-08 13:37:47 -0500

    only shows edit or delete if resource is owned

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
    has_secure_password
    has_one_attached :avatar
-   has_many :reviews
+   has_many :reviews, dependent: :destroy 
    has_many :movies, through: :reviews
-   has_many :lists 
+   has_many :lists, dependent: :destroy 
 
    validates :name, presence: true 
    validates :email, presence: true 
